4f28862bf6866901bcb60acd0b4d3be4410ffed4
ayudya0058 <143370931+ayudya0058@users.noreply.github.com>
2023-12-28 18:56:19 +0700

    Create tambahkan-baru.php

---
+++ tambahkan-baru.php
@@ -0,0 +1,48 @@
+<?php
+include "db_conn.php";
+
+if (isset($_POST["submit"])) {
+   $first_name = $_POST['first_name'];
+   $last_name = $_POST['last_name'];
+   $email = $_POST['email'];
+   $gender = $_POST['gender'];
+
+   $sql = "INSERT INTO `crud`(`id`, `first_name`, `last_name`, `email`, `gender`) VALUES (NULL,'$first_name','$last_name','$email','$gender')";
+
+   $result = mysqli_query($conn, $sql);
+
+   if ($result) {
+      header("Location: index.php?msg=New record created successfully");
+   } else {
+      echo "Failed: " . mysqli_error($conn);
+   }
+}
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <!-- Bootstrap -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+   <!-- Font Awesome -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+   <title>PHP CRUD Application</title>
+</head>
+
+<body>
+   <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #00ff5573;">
+      PHP Complete CRUD Application
+   </nav>
+
+   <div cl
